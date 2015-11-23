bd0cb5a34897ff8ff277a849dc9ac54980234125
kavi87 <e444429@B936474.inetpsa.com>
2015-11-23 17:27:30 +0100

    Added formly module

--- modules/forms.js
+++ modules/forms.js
@@ -1,24 +1,72 @@
 define([
     '{angular}/angular',
     '{angular-formly}/formly',
-    '[text]!{w20-components}/templates/forms/bootstrap/input-template.html',
+    '[text]!{w20-components}/templates/forms/bootstrap/input.html',
+    '[text]!{w20-components}/templates/forms/bootstrap/textarea.html',
+    '[text]!{w20-components}/templates/forms/bootstrap/box.html',
     '[text]!{w20-components}/templates/forms/bootstrap/validation.html',
 
     '{angular-messages}/angular-messages'
 
-], function (angular, formly, inputTemplate, validationTemplate) {
+], function (angular, formly, inputTemplate, textAreaTemplate, boxTemplate, validationTemplate) {
     'use strict';
 
     var module = angular.module('w20ComponentsForms', ['formly', 'ngMessages']);
 
+    module.directive('w20FormAttributes', [function () {
+        return {
+            link: function (scope, element, attrs) {
+
+                attrs.$set('id', scope.id);
+                attrs.$set('formly-dynamic-name', scope.id);
+                attrs.$set('formly-custom-validation', scope.options.validators);
+                attrs.$set('aria-describedby', scope.options.id + '_description');
+                attrs.$set('ng-model', scope.model[scope.options.key]);
+
+                angular.forEach(scope.options.templateOptions, function (value, key) {
+                    if (key === 'required') {
+                        attrs.$set('ng-required', value);
+                    } else if (key === 'disabled') {
+                        attrs.$set('ng-disabled', value);
+                    } else {
+                        attrs.$set(key, value);
+                    }
+                });
+
+            }
+        }
+    }]);
+
     module.run(['formlyConfig', function (formlyConfig) {
 
-        formlyConfig.setType([
+        var types = [
             {
-                name: 'input',
+                name: 'text',
                 template: inputTemplate
+            },
+            {
+                name: 'password',
+                template: inputTemplate
+            },
+            {
+                name: 'date',
+                template: inputTemplate
+            },
+            {
+                name: 'checkbox',
+                template: boxTemplate
+            },
+            {
+                name: 'radio',
+                template: boxTemplate
+            },
+            {
+                name: 'textarea',
+                template: textAreaTemplate
             }
-        ]);
+        ];
+
+        formlyConfig.setType(types);
 
         formlyConfig.setWrapper([
             {
